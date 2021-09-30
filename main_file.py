f42ba57973900f0ea76ca311a99480d805a9af60
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2021-09-30 19:52:07 +0600

    initial plan of user input

--- main_file.py
+++ main_file.py
@@ -1,52 +1,25 @@
 import json
-
-course1 = {
-    'code': 'CSE251',
-    'section1': {
-        'sec_num': 2,
-        'day1': {
-            'day': 'mon',
-            'start': 11.40,
-            'end': 1.20
-        },
-        'day2': {
-            'day': 'wed',
-            'start': 11.40,
-            'end': 1.20
-        },
-        'day3': {
-            'day': 'thur',
-            'start': 4.50,
-            'end': 6.50
-        }
-    },
-    'section2': {
-        'sec_num': 2,
-        'day1': {
-            'day': 'mon',
-            'start': 11.40,
-            'end': 1.20
-        },
-        'day2': {
-            'day': 'wed',
-            'start': 11.40,
-            'end': 1.20
-        },
-        'day3': {
-            'day': 'thur',
-            'start': 4.50,
-            'end': 6.50
-        }
-    }
-}
+# from datetime import datetime
 
 
 if __name__ == '__main__':
 
-    course_number = int(input())
-    courses = []
-    for i in range(course_number):
-        course_code = input("Course Code: ")
-        courses.append({course_code: course1})
+    total_courses = int(input("Total Courses: "))
+    courses = {}
+    for course in range(total_courses):
+        course_code = input(f"Course Code({course + 1}): ")
+        total_day = int(input(f"Total classes in a week of {course_code}: "))
+        total_section = int(input(f"Total Section of {course_code}: "))
+        section_dict = {}
+        for section in range(total_section):
+            section_number = int(input(f"{section+1}. Section number of {course_code}: "))
+            days_dict = {}
+            for days in range(total_day):
+                day = input("Day: ")
+                start_time = input("Start Time: ")
+                end_time = input("End Time: ")
+                days_dict[day] = [start_time, end_time]
+            section_dict[section_number] = days_dict
+        courses[course_code] = section_dict
 
     print(json.dumps(courses, indent=4))
