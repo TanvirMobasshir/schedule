6579349597bb832b83141a05261f2ad357d192d2
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2021-09-30 20:09:06 +0600

    user_input() function

--- main_file.py
+++ main_file.py
@@ -1,25 +1,32 @@
 import json
-# from datetime import datetime
 
 
-if __name__ == '__main__':
-
+def user_input():
     total_courses = int(input("Total Courses: "))
     courses = {}
     for course in range(total_courses):
         course_code = input(f"Course Code({course + 1}): ")
         total_day = int(input(f"Total classes in a week of {course_code}: "))
-        total_section = int(input(f"Total Section of {course_code}: "))
+        total_section = int(input(f"Total Section(s) of {course_code}: "))
         section_dict = {}
         for section in range(total_section):
-            section_number = int(input(f"{section+1}. Section number of {course_code}: "))
-            days_dict = {}
+            section_number = int(
+                input(f"{section + 1}. Section number of {course_code}: "))
+            days_dict = []
             for days in range(total_day):
                 day = input("Day: ")
                 start_time = input("Start Time: ")
                 end_time = input("End Time: ")
-                days_dict[day] = [start_time, end_time]
+                temp_dict = {day: [start_time, end_time]}
+                days_dict.append(temp_dict)
             section_dict[section_number] = days_dict
         courses[course_code] = section_dict
 
-    print(json.dumps(courses, indent=4))
+    return courses
+
+
+if __name__ == '__main__':
+
+    main_dict = user_input()
+
+    print(json.dumps(main_dict, indent=4))
